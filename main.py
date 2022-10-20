651e84e8792433bb722cef92fbae15c6b7f0cc15
Slikek <73469414+Av10n@users.noreply.github.com>
2022-10-20 21:11:39 +0300

    experiments with "send_photo"...

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ bot = telebot.TeleBot(script.token)
 def start(message):
     bot.send_message(message.chat.id, 'Hi!, I\'m AWED\'s bot!')
 
+@bot.message_handler(commands=['sendMeImage'])
+def image(message):
+    bot.send_photo(message.chat.id, 'https://cdn.discordapp.com/attachments/1032285013644947486/1032716976205877248/animeArt.jpg')
+
 @bot.message_handler(content_types='text')
 def message(message):
     bot.send_message(message.chat.id, message.text)
